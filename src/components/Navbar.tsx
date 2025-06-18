341f959c04c63b981b540fa832225767381f85e2
Marius Rønning <mpr941@hotmail.com>
2025-06-18 10:44:41 +0200

    FEAT: added sceloton for navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,14 +2,22 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="bg-white shadow px-6 py-4 flex gap-6 items-center">
+    <nav className="flex justify-between items-center px-6 py-4 bg-[#f3eee9] border-b border-gray-300">
+    {/* Logo + Firmanavn */}
+    <div className="flex items-center gap-2">
+      <img src="/icon/dola-logo.png" alt="Døla takst logo" className="h-8 w-auto" />
+      <span className="text-xl font-semibold text-black">Døla takst</span>
+    </div>
+
+    {/* Navigasjonslenker */}
+    <div className="flex gap-8 text-black text-sm font-medium">
       <NavLink to="/">Hjem</NavLink>
-
+      <NavLink to="/tjenester">Tjenester</NavLink>
       <NavLink to="/om-oss">Om oss</NavLink>
-
       <NavLink to="/kontakt">Kontakt</NavLink>
-    </nav>
-  );
+    </div>
+  </nav>
+  )
 };
 
 export default Navbar;
