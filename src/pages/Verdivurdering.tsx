a8ba02fea6ddc8d47c11ca5866fb858797f89172
Marius Rønning <mpr941@hotmail.com>
2025-06-18 13:30:12 +0200

    FEAT: content for new section

--- src/pages/Verdivurdering.tsx
+++ src/pages/Verdivurdering.tsx
@@ -1,7 +1,11 @@
 import VerdivurderingHero from "../components/verdivurdering/VerdivurderingHero";
+import VerdivurderingInnhold from "../components/verdivurdering/VerdivurderingInnhold";
 
 export default function Verdivurdering(){
     return(
+        <>
         <VerdivurderingHero/>
+        <VerdivurderingInnhold/>
+        </>
     )
 }
