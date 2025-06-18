963ea9771ee7f92a0c49abdd39081a21aadee857
Marius Rønning <mpr941@hotmail.com>
2025-06-18 14:30:39 +0200

    FEAT: Added last section

--- src/pages/Tilstand.tsx
+++ src/pages/Tilstand.tsx
@@ -1,3 +1,5 @@
+import ContactSection from "../components/home/ContactSection";
+import TilstandFordeler from "../components/tilstand/TilstandFordeler";
 import TilstandHero from "../components/tilstand/TilstandHero";
 import TilstandsrapportInfo from "../components/tilstand/TilstandsRapportInfo";
 
@@ -6,6 +8,8 @@ export default function Tilstand(){
         <>
         <TilstandHero/>
         <TilstandsrapportInfo/>
+        <TilstandFordeler/>
+        <ContactSection/>
         </>
     )
 }
