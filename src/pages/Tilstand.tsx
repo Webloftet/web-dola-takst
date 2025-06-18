329e34af9a49e9efbf7d412098ce4e5e7c3ab594
Marius Rønning <mpr941@hotmail.com>
2025-06-18 14:51:28 +0200

    FIX: forgot to pull from main

--- src/pages/Tilstand.tsx
+++ src/pages/Tilstand.tsx
@@ -1,7 +1,7 @@
 import ContactSection from "../components/home/ContactSection";
 import TilstandFordeler from "../components/tilstand/TilstandFordeler";
 import TilstandHero from "../components/tilstand/TilstandHero";
-import TilstandsrapportInfo from "../components/tilstand/TilstandsRapportInfo";
+import TilstandsrapportInfo from "../components/tilstand/TilstandsrapportInfo";
 
 export default function Tilstand(){
     return(
