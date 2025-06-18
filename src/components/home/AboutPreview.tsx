16d4ddb79405e23e0da81305c21ca6a820ea47c0
Marius Rønning <mpr941@hotmail.com>
2025-06-18 13:09:53 +0200

    FEAT: added footer content contactsection

--- src/components/home/AboutPreview.tsx
+++ src/components/home/AboutPreview.tsx
@@ -5,13 +5,13 @@ export default function AboutPreview() {
   return (
     <section className="bg-back px-6 py-16 text-black">
       <div className="max-w-6xl mx-auto flex flex-col md:flex-row items-center gap-10">
-        {/* Tekstinnhold */}
         <div className="flex-1">
-          <h2 className="text-xl md:text-2xl font-bold font-primary mb-4">
+          <h2 className="text-xl md:text-3xl font-bold font-primary mb-4">
             Om oss
           </h2>
           <p className="text-sm md:text-base font-light max-w-md">
-            Vi er ett erfarent takstfirma med base i Oslo, og har spesialisert oss på verdi og tilstandsvurdering av boliger og næringseiendom
+            Vi er ett erfarent takstfirma med base i Oslo, og har spesialisert
+            oss på verdi og tilstandsvurdering av boliger og næringseiendom
           </p>
 
           <NavLink
@@ -23,12 +23,11 @@ export default function AboutPreview() {
           </NavLink>
         </div>
 
-        {/* Bilde */}
         <div className="flex-1">
           <img
             src="/img/om-oss.png"
             alt="Om oss"
-            className="w-full h-auto object-contain"
+            className="w-full h-50 object-contain hidden md:flex"
           />
         </div>
       </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,17 @@
+import AboutPreview from "../components/home/AboutPreview";
+import ContactSection from "../components/home/ContactSection";
 import Hero from "../components/home/Hero";
 import Services from "../components/home/Services";
 import WhyChooseUs from "../components/home/WhyChooseUs";
 
-export default function Homepage(){
-    return(
-        <>
-        <Hero/>
-        <WhyChooseUs />
-        <Services/>
-        </>
-    )
+export default function Homepage() {
+  return (
+    <>
+      <Hero />
+      <WhyChooseUs />
+      <Services />
+      <AboutPreview />
+      <ContactSection/>
+    </>
+  );
 }
