ae141133e38fd7b608d8d647dbda160202094b03
Marius Rønning <mpr941@hotmail.com>
2025-06-18 11:06:36 +0200

    FEAT: added new styling updated navbar with new styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,15 +2,15 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-between items-center px-6 py-4 bg-[#f3eee9] border-b border-gray-300">
+    <nav className="relative flex items-center px-6 py-4 bg-back">
     {/* Logo + Firmanavn */}
     <div className="flex items-center gap-2">
       <img src="/icon/dola-logo.png" alt="Døla takst logo" className="h-8 w-auto" />
-      <span className="text-xl font-semibold text-black">Døla takst</span>
+      <span className="text-xl font-primary font-bold text-black">Døla takst</span>
     </div>
 
     {/* Navigasjonslenker */}
-    <div className="flex gap-8 text-black text-sm font-medium">
+    <div className="absolute left-1/2 transform -translate-x-1/2 flex gap-8 text-black text-sm font-primary font-semibold">
       <NavLink to="/">Hjem</NavLink>
       <NavLink to="/tjenester">Tjenester</NavLink>
       <NavLink to="/om-oss">Om oss</NavLink>
