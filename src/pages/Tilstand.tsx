368523d985ef0bf0c54670fd2a5a20fd8c6a1e3a
Marius Rønning <mpr941@hotmail.com>
2025-06-18 14:36:14 +0200

    FIX: fixed path error

--- src/pages/Tilstand.tsx
+++ src/pages/Tilstand.tsx
@@ -1,7 +1,8 @@
 import ContactSection from "../components/home/ContactSection";
 import TilstandFordeler from "../components/tilstand/TilstandFordeler";
 import TilstandHero from "../components/tilstand/TilstandHero";
-import TilstandsrapportInfo from "../components/tilstand/TilstandsRapportInfo";
+import TilstandsrapportInfo from "../components/tilstand/TilstandsrapportInfo";
+
 
 export default function Tilstand(){
     return(
