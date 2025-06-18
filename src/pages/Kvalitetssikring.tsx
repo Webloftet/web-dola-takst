3fe4357775b8da41e2fd3d724a50d49f15a6191f
Marius Rønning <mpr941@hotmail.com>
2025-06-18 13:49:26 +0200

    FEAT: included kompontes prew used

--- src/pages/Kvalitetssikring.tsx
+++ src/pages/Kvalitetssikring.tsx
@@ -1,11 +1,17 @@
+import ContactSection from "../components/home/ContactSection";
 import HvaErKvalitetssikring from "../components/kvalitetssikring/HvaErKvalitetssikring";
 import KvalitetssikringHero from "../components/kvalitetssikring/KvalitetssikringHero";
+import NarTrengerDuDette from "../components/kvalitetssikring/NarTrenger";
+import SlikJobberVi from "../components/verdivurdering/SlikJobberVi";
 
-export default function Kvalitetssikring(){
-    return(
-        <>
-        <KvalitetssikringHero/>
-        <HvaErKvalitetssikring/>
-        </>
-    )
+export default function Kvalitetssikring() {
+  return (
+    <>
+      <KvalitetssikringHero />
+      <HvaErKvalitetssikring />
+      <NarTrengerDuDette />
+      <SlikJobberVi />
+      <ContactSection/>
+    </>
+  );
 }
